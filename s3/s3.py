fbaaee365f1e2d2f7c696829533c46c0c411b6a1
Cliff Boan <cliffdboan@gmail.com>
2024-06-20 13:38:32 -0400

    add s3 bucket creation

---
+++ s3/s3.py
@@ -0,0 +1,21 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+def create_bucket(bucket_name, region = None):
+    """create a new bucket in the specified region.
+    If region is left as None, the default region is US-East-1
+    """
+
+    try:
+        if region is None:
+            client = boto3.resource('s3')
+            client.create_bucket(Bucket = bucket_name)
+        else:
+            client = boto3.resource('s3', region_name = region)
+            location = {'LocationRestraint': region}
+            client.create_bucket(Bucket = bucket_name, CreateBucketConfiguration = location)
+    except ClientError as e:
+        print("Error! {}".format(str(e)))
+
+    print("Bucket {} created!".format(bucket_name))
